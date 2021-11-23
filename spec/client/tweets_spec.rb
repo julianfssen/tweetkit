33e54bf6dc102dbee5b52eedb66a65a62765bb55
Ryan Laughlin <rofreg@gmail.com>
2021-11-23 11:54:43 -0500

    Update tests

--- spec/client/tweets_spec.rb
+++ spec/client/tweets_spec.rb
@@ -1,31 +1,41 @@
 require_relative '../spec_helper'
 
 describe Tweetkit::Client::Tweets do
-  before(:each) do
-    @client = Tweetkit::Client.new(bearer_token: ENV['BEARER_TOKEN'])
-  end
+  client_types = {
+    bearer_token: Tweetkit::Client.new(bearer_token: ENV['BEARER_TOKEN']),
+    access_token: Tweetkit::Client.new(
+      consumer_key: ENV['CONSUMER_KEY'],
+      consumer_secret: ENV['CONSUMER_SECRET'],
+      access_token: ENV['ACCESS_TOKEN'],
+      access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+    ),
+  }
 
-  describe '.tweet' do
-    it 'accepts an ID and gets a tweet' do
-      response = @client.tweet(1228393702244134912)
+  client_types.each do |client_type, client|
+    context "with #{client_type} client" do
+      describe '.tweet' do
+        it 'accepts an ID and gets a tweet' do
+          response = client.tweet(1228393702244134912)
 
-      expect(response.tweet.text).not_to be_empty
-    end
-  end
+          expect(response.tweet.text).not_to be_empty
+        end
+      end
 
-  describe '.tweets' do
-    it 'accepts an array of IDs and gets the tweets' do
-      response = @client.tweets([1228393702244134912, 1227640996038684673])
+      describe '.tweets' do
+        it 'accepts an array of IDs and gets the tweets' do
+          response = client.tweets([1228393702244134912, 1227640996038684673])
 
-      expect(response.tweets.size).to eq(2)
-      expect(response.tweets.first.text).not_to be_empty
-    end
+          expect(response.tweets.size).to eq(2)
+          expect(response.tweets.first.text).not_to be_empty
+        end
 
-    it 'accepts comma-separated string of IDs and gets the tweets' do
-      response = @client.tweets([1228393702244134912, 1227640996038684673])
+        it 'accepts comma-separated string of IDs and gets the tweets' do
+          response = client.tweets([1228393702244134912, 1227640996038684673])
 
-      expect(response.tweets.size).to eq(2)
-      expect(response.tweets.first.text).not_to be_empty
+          expect(response.tweets.size).to eq(2)
+          expect(response.tweets.first.text).not_to be_empty
+        end
+      end
     end
   end
 end
